ee83be0fd434c1f15bbc0b02ec65c52daadda8e0
Simon <simon.moro@imt-atlantique.net>
2020-12-09 11:24:32 +0100

    ajout du plugin traduisant les lyrics, sauf dans le cas où la chanson est en anglais à l'origine

--- packages/core/src/plugins/lyrics/index.ts
+++ packages/core/src/plugins/lyrics/index.ts
@@ -1,2 +1,3 @@
 export { default as SimpleLyricsProvider } from './simple';
 export { default as TranslateLyricsProvider } from './translate';
+export { default as TranslateNotEngLyricsProvider } from './translate_exceptEng';
---
+++ packages/core/src/plugins/lyrics/translate_exceptEng.ts
@@ -0,0 +1,39 @@
+import logger from 'electron-timber';
+import lyrics from 'simple-get-lyrics';
+import translate from 'google-translate-open-api';
+import { getOption } from '../../index';
+
+import LyricsProvider from '../lyricsProvider';
+
+class TranslateNotEngLyricsProvider extends LyricsProvider {
+  constructor() {
+    super();
+    this.name = 'Translate Lyrics (except English) Provider Plugin';
+    this.sourceName = 'Translate (except English) Lyrics Provider';
+    this.description = 'Translate lyrics provider plugin. Uses several sources with fallbacks. Songs with english lyrics will not be translated';
+    this.image = null;
+    this.isDefault = false;
+  }
+
+  search(artistName: string, trackName: string): string {
+    const toTranslate = getOption('language').split('-')[0].split('_')[0];
+
+    return lyrics.search(artistName, trackName)
+      .then(result => {
+        return translate(result.lyrics, {to: toTranslate})
+          .then(res => { 
+            if (res.data[1] === toTranslate) {
+              return result.lyrics; 
+            }
+            return res.data[0]; 
+          }).catch(function (err) {
+            logger.log('error', err); return result.lyrics;
+          });
+      })
+      .catch(function (err) {
+        logger.log('error', err);
+      });
+  }
+}
+
+export default TranslateNotEngLyricsProvider;
